6187f35bba2ea2d024f522024ac976884985b636
Anton Vyshkin <a.vyshkin@tinkoff.ru>
2023-04-02 21:58:53 +0300

    - add intents - reformat code

--- app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
@@ -2,8 +2,10 @@ package com.example.playlistmaker
 
 import android.annotation.SuppressLint
 import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.FrameLayout
 import android.widget.ImageView
 
 class SettingsActivity : AppCompatActivity() {
@@ -12,9 +14,39 @@ class SettingsActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
         val backButton = findViewById<ImageView>(R.id.back_button)
-        backButton.setOnClickListener{
-            val backButtonIntent = Intent(this,MainActivity::class.java)
+        backButton.setOnClickListener {
+            val backButtonIntent = Intent(this, MainActivity::class.java)
             startActivity(backButtonIntent)
+        }
+        val sharingButton = findViewById<FrameLayout>(R.id.sharing_button)
+        sharingButton.setOnClickListener {
+            val sharingButtonIntent = Intent(Intent.ACTION_SEND).apply {
+                putExtra(
+                    Intent.EXTRA_TEXT,
+                    "https://practicum.yandex.ru/android-developer/"
+                )
+                type = "text/plain"
+            }
+            startActivity(sharingButtonIntent)
+        }
+        val writeToHelpDeskButton = findViewById<FrameLayout>(R.id.write_help_desk)
+        writeToHelpDeskButton.setOnClickListener {
+            val title = getString(R.string.help_desk_post_title)
+            val message = getString(R.string.help_desk_post_text)
+            val email = getString(R.string.help_desk_post_email)
+            val writeToHelpDeskIntent = Intent(Intent.ACTION_SENDTO).apply {
+                data = Uri.parse("mailto:")
+                putExtra(Intent.EXTRA_EMAIL, arrayOf(email))
+                putExtra(Intent.EXTRA_SUBJECT, title)
+                putExtra(Intent.EXTRA_TEXT, message)
+            }
+            startActivity(writeToHelpDeskIntent)
+        }
+        val licenseAgreementButton = findViewById<FrameLayout>(R.id.license_agreement)
+        licenseAgreementButton.setOnClickListener {
+            val licenseAgreementIntent = Intent(Intent.ACTION_VIEW)
+            licenseAgreementIntent.data = Uri.parse("https://yandex.ru/legal/practicum_offer/")
+            startActivity(licenseAgreementIntent)
         }
     }
 }
